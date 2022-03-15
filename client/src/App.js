fcca4557790f482251de74d645aec791137bd06f
J Lee <jay.j.lee.1@gmail.com>
2022-03-15 15:27:48 -0400

    app page initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
+
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  function handleLogOutClick(){
+    fetch("/logout",{
+        method: "DELETE"
+    }).then((r) => {
+        if(r.ok){
+            setUser(null);
+        }
+    });
+    // Navigate to home page after logout and clear history
+    navigate("/");
+}
+
+  if (!user) return (
+    <>
+    <Container>
+      <Alert className="mt-3" variant="primary" >Please Login OR Signup To Create A New Account</Alert>
+    </Container>
+    <Login onLogin={setUser}/>
+    </>
+  )
   return (
     <div className="App">
       <header className="App-header">
