e93d58c9bea3a3b1c648bdff82c0a5bf2c866c2c
J Lee <jay.j.lee.1@gmail.com>
2022-03-15 15:02:44 -0400

    added admin to user

--- db/migrate/20220315190200_create_users.rb
+++ db/migrate/20220315190200_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :password_digest
       t.string :email
+      t.boolean :admin
 
       t.timestamps
     end
